2ba56d09a4424ca654d34e5c15c3ae80d99071c0
Austin Nario <austnari@email.unc.edu>
2021-08-26 15:03:22 -0400

    Finished exercise 01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -2,7 +2,6 @@
 
 __author__ = "730396074"
 
-
 name: str = input("What is your name? ")
 print("Nice to meet you, " + name + ", you look lovely today!")
 print( name + " is probably the best name and person I've ever heard or seen.")
